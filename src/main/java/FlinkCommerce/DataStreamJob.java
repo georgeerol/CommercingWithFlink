b0e644e38ca4495684b3609d01287a74889b6c07
george <fouliex@gmail.com>
2024-02-20 22:05:08 -0800

    fix: update streaming to elasticsearch

--- src/main/java/FlinkCommerce/DataStreamJob.java
+++ src/main/java/FlinkCommerce/DataStreamJob.java
@@ -251,9 +251,10 @@ public class DataStreamJob {
                                     .source(json, XContentType.JSON);
                             requestIndexer.add(indexRequest);
                         })
+                        .setBulkFlushMaxActions(2)         // Set maximum number of actions per bulk request
+                        .setBulkFlushInterval(1000L)
                         .build()
         ).name("Elasticsearch Sink");
-
         // Execute program, beginning computation.
         env.execute("Flink Ecommerce Realtime Streaming");
     }
